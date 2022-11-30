2ed9ecdf625cc6288c21d052950ef97191799a32
LIM WEIJIE <limweijie_95@hotmail.com>
2022-12-01 03:42:42 +0800

    update static features of html

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify, render_template
-from flask_cors import cross_origin
-import ktrain
+#from flask_cors import cross_origin
+#import ktrain
 
 app = Flask(__name__)
-model = ktrain.load_predictor('distilbert')
+#model = ktrain.load_predictor('distilbert')
 
 
 @app.route("/")
-@cross_origin()
+#@cross_origin()
 def home():
     return render_template("index.html")
 
 
 @app.route("/predict", methods=["GET", "POST"])
-@cross_origin()
+#@cross_origin()
 def predict():
     pass
 
