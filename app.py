fb4526bf3eb5fdef395cfb89cb94f8e171fad01d
LIM WEIJIE <limweijie_95@hotmail.com>
2022-12-01 03:36:15 +0800

    update html, app.py and stylesheet

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import cross_origin
+import ktrain
+
+app = Flask(__name__)
+model = ktrain.load_predictor('distilbert')
+
+
+@app.route("/")
+@cross_origin()
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET", "POST"])
+@cross_origin()
+def predict():
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
