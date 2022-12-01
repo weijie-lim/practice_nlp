b69f83dfbdbd6c5e5736525b1d12e773df6fe69f
LIM WEIJIE <limweijie_95@hotmail.com>
2022-12-01 23:42:32 +0800

    update for local server

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin
 import json
-# import ktrain
+import ktrain
 # import os
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-# model = ktrain.load_predictor('distilbert')
+model = ktrain.load_predictor('distilbert')
 
 app = Flask(__name__)
 
@@ -22,23 +22,23 @@ def predict():
         
         para = None
         if request.method == 'POST':
-            # para = request.form['data']
-            # predicted = model.predict(para)
-            # probabilities = model.predict(para, return_proba=True)
-            # probabilities = list(probabilities)
+            para = request.form['data']
+            predicted = model.predict(para)
+            probabilities = model.predict(para, return_proba=True)
+            probabilities = list(probabilities)
             
-            # result = {}
-            # result["response"] = "200"
-            # result["prediction"] = predicted
-            # cat = model.get_classes()
-            # #cat = ["waivers", "governing laws", "amendments", "counterparts", "warranties", "terminations", "valid issuances", "government regulations", "trade relations", "trading activities"]
-            # for i in range(len(probabilities)):
-            #     result[cat[i]] = "{:.5f}".format(probabilities[i])
+            result = {}
+            result["response"] = "200"
+            result["prediction"] = predicted
+            cat = model.get_classes()
+            #cat = ["waivers", "governing laws", "amendments", "counterparts", "warranties", "terminations", "valid issuances", "government regulations", "trade relations", "trading activities"]
+            for i in range(len(probabilities)):
+                result[cat[i]] = "{:.5f}".format(probabilities[i])
             
-            # return json.dumps(result)
+            return json.dumps(result)
             
-            #use if memory exceeded
-            return json.dumps({"response": "503", "prediction" : "The Distilbert Model used takes up too much memory. To proceed, please clone repo to test on local server: ", "github": "https://github.com/weijie-lim/practice_nlp"})
+            # #use if memory exceeded
+            # return json.dumps({"response": "503", "prediction" : "The Distilbert Model used takes up too much memory. To proceed, please clone repo to test on local server: ", "github": "https://github.com/weijie-lim/practice_nlp"})
     except:
         return('Error 500: Service is currently down. We are currently looking into it!', 400)
 
