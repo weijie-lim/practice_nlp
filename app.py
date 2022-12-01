3e29269f11841a409124a882ba9b2e68f880218b
LIM WEIJIE <limweijie_95@hotmail.com>
2022-12-01 16:26:59 +0800

    resolved all errors with ajax and updated html with table

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin
-import sys
 import ktrain
+import json
 
 app = Flask(__name__)
-#model = ktrain.load_predictor('distilbert')
-
+model = ktrain.load_predictor('distilbert')
 
 @app.route("/")
 @cross_origin()
@@ -16,13 +15,25 @@ def home():
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    para = None
-    if request.method == 'POST':
-        print(request.form)
-        return('', 204)
-        #para = request.json['data']
-    #print('Hello world!', file=sys.stderr)
-    # return render_template("index.html")
+    try:
+        para = None
+        if request.method == 'POST':
+            para = request.form['data']
+            predicted = model.predict(para)
+            probabilities = model.predict(para, return_proba=True)
+            probabilities = list(probabilities)
+            
+            result = {}
+            result["response"] = "200"
+            result["prediction"] = predicted
+            cat = model.get_classes()
+            #cat = ["waivers", "governing laws", "amendments", "counterparts", "warranties", "terminations", "valid issuances", "government regulations", "trade relations", "trading activities"]
+            for i in range(len(probabilities)):
+                result[cat[i]] = "{:.5f}".format(probabilities[i])
+            
+            return json.dumps(result)
+    except:
+        return('Error 500: Service is currently down. We are currently looking into it!', 400)
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -3,6 +3,23 @@ $("#send").click(function(){
   $.ajax({
     type : 'POST',
     url : "predict",
-    data : {'data':para}
-  });
+    data : {'data':para},
+  }).then(
+    function (response) {
+      var response = JSON.parse(response);
+      $('#category').html("Category: " + response['prediction']);
+      $('#probability').html("Probability: " + response[response['prediction']]);
+      $('#amendments').html(response['amendments']);
+      $('#counterparts').html(response['counterparts']);
+      $('#laws').html(response['governing laws']);
+      $('#reg').html(response['government regulations']);
+      $('#terminations').html(response['terminations']);
+      $('#trade_relations').html(response['trade relations']);
+      $('#trading_activities').html(response['trading activities']);
+      $('#valid_issuances').html(response['valid issuances']);
+      $('#waivers').html(response['waivers']);
+      $('#warranties').html(response['warranties']);
+      console.log(response)
+    }
+  );
 });
