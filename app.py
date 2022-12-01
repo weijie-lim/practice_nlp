a011d7db62e73504ba5cd972b5d563d18656b17b
LIM WEIJIE <limweijie_95@hotmail.com>
2022-12-01 23:37:21 +0800

    modify template for mobile screens

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin
-import ktrain
 import json
+# import ktrain
 # import os
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# model = ktrain.load_predictor('distilbert')
 
 app = Flask(__name__)
-model = ktrain.load_predictor('distilbert')
 
 @app.route("/")
 @cross_origin()
@@ -22,23 +22,23 @@ def predict():
         
         para = None
         if request.method == 'POST':
-            para = request.form['data']
-            predicted = model.predict(para)
-            probabilities = model.predict(para, return_proba=True)
-            probabilities = list(probabilities)
+            # para = request.form['data']
+            # predicted = model.predict(para)
+            # probabilities = model.predict(para, return_proba=True)
+            # probabilities = list(probabilities)
             
-            result = {}
-            result["response"] = "200"
-            result["prediction"] = predicted
-            cat = model.get_classes()
-            #cat = ["waivers", "governing laws", "amendments", "counterparts", "warranties", "terminations", "valid issuances", "government regulations", "trade relations", "trading activities"]
-            for i in range(len(probabilities)):
-                result[cat[i]] = "{:.5f}".format(probabilities[i])
+            # result = {}
+            # result["response"] = "200"
+            # result["prediction"] = predicted
+            # cat = model.get_classes()
+            # #cat = ["waivers", "governing laws", "amendments", "counterparts", "warranties", "terminations", "valid issuances", "government regulations", "trade relations", "trading activities"]
+            # for i in range(len(probabilities)):
+            #     result[cat[i]] = "{:.5f}".format(probabilities[i])
             
-            return json.dumps(result)
+            # return json.dumps(result)
             
-            # #use if memory exceeded
-            # return json.dumps({"prediction" : "Distilbert Model too large, please clone github repo to test locally"})
+            #use if memory exceeded
+            return json.dumps({"response": "503", "prediction" : "The Distilbert Model used takes up too much memory. To proceed, please clone repo to test on local server: ", "github": "https://github.com/weijie-lim/practice_nlp"})
     except:
         return('Error 500: Service is currently down. We are currently looking into it!', 400)
 
--- static/main.js
+++ static/main.js
@@ -7,18 +7,24 @@ $("#send").click(function(){
   }).then(
     function (response) {
       var response = JSON.parse(response);
-      $('#category').html("Category: " + response['prediction']);
-      $('#probability').html("Probability: " + response[response['prediction']]);
-      $('#amendments').html(response['amendments']);
-      $('#counterparts').html(response['counterparts']);
-      $('#laws').html(response['governing laws']);
-      $('#reg').html(response['government regulations']);
-      $('#terminations').html(response['terminations']);
-      $('#trade_relations').html(response['trade relations']);
-      $('#trading_activities').html(response['trading activities']);
-      $('#valid_issuances').html(response['valid issuances']);
-      $('#waivers').html(response['waivers']);
-      $('#warranties').html(response['warranties']);
+      if (response['response'] == "503"){
+        $('#category').html(response['prediction'] + "<a href=\"" + response['github'] + "\">Github Link</a>");
+        $('#probability').html("");
+      } else {
+        $('#category').html("Category: " + response['prediction']);
+        $('#probability').html("Probability: " + response[response['prediction']]);
+        $('#amendments').html(response['amendments']);
+        $('#counterparts').html(response['counterparts']);
+        $('#laws').html(response['governing laws']);
+        $('#reg').html(response['government regulations']);
+        $('#terminations').html(response['terminations']);
+        $('#trade_relations').html(response['trade relations']);
+        $('#trading_activities').html(response['trading activities']);
+        $('#valid_issuances').html(response['valid issuances']);
+        $('#waivers').html(response['waivers']);
+        $('#warranties').html(response['warranties']);
+      }
+
       console.log(response)
     }
   );
